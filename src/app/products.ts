77bfef867812b7f23692dd3d2a47c3dbbde18bb1
Josep <arpij@ryanair.com>
2020-03-02 13:05:59 +0100

    Finish tutorial

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,24 @@
 export const products = [
   {
     name: 'Phone XL',
+    slug: 'Phone_XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    id: 0
   },
   {
     name: 'Phone Mini',
+    slug: 'Phone_Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    id: 1
   },
   {
     name: 'Phone Standard',
+    slug: 'Phone_Standard',
     price: 299,
-    description: ''
+    description: '',
+    id: 2
   }
 ];
 
